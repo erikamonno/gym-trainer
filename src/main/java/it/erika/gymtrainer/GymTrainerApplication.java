0f469eae97663dd561cb39bd87d43e3da1f387c9
Erika Monno <erika.monno97@gmail.com>
2025-04-11 19:57:45 +0200

    first commit

---
+++ src/main/java/it/erika/gymtrainer/GymTrainerApplication.java
@@ -0,0 +1,13 @@
+package it.erika.gymtrainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymTrainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymTrainerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/erika/gymtrainer/GymTrainerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.erika.gymtrainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymTrainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
